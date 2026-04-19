410a28a7a097f5d63b4a65de51eb6a70f573d841
Manu(Manu11-Pro) <194583214+Manu11-Pro@users.noreply.github.com>
2026-04-19 11:56:20 +0530

    Refactor: Added and improved folder gui and functionality

--- src/file_organiser/gui.py
+++ src/file_organiser/gui.py
@@ -3,11 +3,10 @@ from tkinter import filedialog
 
 all_files_paths = tk.filedialog.askopenfilenames(title="Select files to be organised", filetypes=[("All files", "*.*")])
 
-all_folders_paths = filedialog.askdirectory(title="Select folders as the destination for organised files")
-
 print(all_files_paths)
 
 for files_path in all_files_paths:
     print(files_path)
+    all_folders_paths = filedialog.askdirectory(title="Select folders as the destination for organised files")
 
 print(all_folders_paths)
